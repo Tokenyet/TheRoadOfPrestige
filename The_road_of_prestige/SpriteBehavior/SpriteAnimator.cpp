20618f0824e86ce0df3473958d67393d51d7ff08
Dowen <terryden5566@yahoo.com.tw>
2015-08-16 21:17:56 +0800

    Init Project

---
+++ The_road_of_prestige/SpriteBehavior/SpriteAnimator.cpp
@@ -0,0 +1,47 @@
+#include "SpriteAnimator.h"
+
+
+SpriteAnimator::SpriteAnimator(const char* path)
+{
+	_plist = path;
+	_cache = SpriteFrameCache::getInstance();
+	if (CCFileUtils::getInstance()->isFileExist(path))
+		_cache->addSpriteFramesWithFile(path);
+	else
+		throw ERROR_FILE_EXISTS;
+}
+
+SpriteAnimator* SpriteAnimator:: Create(const char* path)
+{
+	return new SpriteAnimator(path);
+}
+
+
+SpriteAnimator::~SpriteAnimator()
+{
+	_cache->removeSpriteFramesFromFile(_plist);
+	_namedActions.clear();
+}
+
+
+void SpriteAnimator::SetAnimation(const char* name, int amount, float delay)
+{
+	Vector<SpriteFrame*> animFrames(amount);
+	char front[100] = { 0 };
+	char str[100] = { 0 };
+	for (int i = 0; i < amount; i++)
+	{
+		strcpy(front, name);
+		sprintf(str,"%d.png", i);
+		strcat(front, str);
+		auto frame = _cache->getSpriteFrameByName(front);
+		animFrames.pushBack(frame);
+	}
+	auto animation = Animation::createWithSpriteFrames(animFrames , delay);
+	_namedActions.insert(name,Animate::create(animation));
+}
+
+Animate* SpriteAnimator::GetSpecifiedAnimate(const char* name)
+{
+	return _namedActions.at(name);
+}
---
+++ The_road_of_prestige/SpriteBehavior/SpriteAnimator.h
@@ -0,0 +1,51 @@
+#ifndef  _SPRITE_ANIMATOR_H_
+#define  _SPRITE_ANIMATOR_H_
+
+#include "cocos2d.h"
+using namespace cocos2d;
+
+// ===================================================================================
+// This class is to help sprite to create animation for Sprite.runAction.
+// 
+// Feature:
+// SetAnimation : SetAnimation("attack",5,0.3)
+//
+// Requirement:
+// Need a .plist with same filename .png on the same folder
+// Need a order png name [ name0.png ~ nameXXX.png ] in the .plist.
+// -- 
+// Usage:
+// 	SpriteAnimator attackAnimator("human/bat.plist");
+//  attackAnimator.SetAnimation("attack", 6, 0.3);
+//  auto tempAnimate = attackAnimator.GetSpecifiedAnimate("attack");
+//  auto bats = Sprite::create();
+//  bats->runAction(Repeat::create(tempAnimate, 10));
+// ***********************************************************************************
+// Maintain by Dowen.
+// ===================================================================================
+
+class SpriteAnimator
+{
+public:
+	SpriteAnimator(const char* path);
+	~SpriteAnimator();
+	// Create pointer with class
+	SpriteAnimator* Create(const char* path);
+	// name  : png name in .plist
+	// amount: how many .png with name
+	// delay : time to change frame
+	void SetAnimation(const char* name, int amount, float delay);
+	// return Animation you want
+	Animate* GetSpecifiedAnimate(const char* name);
+protected:
+	// Spritsheet store in this cache
+	SpriteFrameCache* _cache;
+	// all animation work are in this stack
+	//Map<String, Vector<SpriteFrame*>> _namedActions;
+	Map<const char*, Animate*> _namedActions;
+
+private:
+	const char* _plist;
+};
+
+#endif
